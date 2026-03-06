efac63492fbc074d6e2744303e01cc5934fa7d4e
Kamil <kamil6395@gmil.com>
2026-03-06 10:25:25 +0100

    Dodano User SignIn Route, Polaczenie do MongoDB. Poprawnie skonfigurowany app.js

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//Env variables
+const dotenv = require('dotenv');
+dotenv.config();
 
-var app = express();
+//MongoDB connection
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI)
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+const db = mongoose.connection;
+console.log("Connecting");
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//Cors configuration
+app.use(cors());
+app.use(express.json());
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/', (req, res) => {
+  res.send('Legalne Lowiska Backend is running!');
+});
+
+app.use('/api/users/signin', require('./routes/users/signin'));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 module.exports = app;
