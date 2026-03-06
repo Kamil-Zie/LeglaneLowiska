7b14b3dc16593ea8141f0b264dc736b65758d9bb
Kamil <kamil6395@gmil.com>
2026-03-06 17:47:31 +0100

    Test webpage.

---
+++ src/components/WebPage/index.jsx
@@ -0,0 +1,11 @@
+const WebPage = () => {
+    return (
+        <div>
+            <h1>Witamy na stronie Legalne Łowiska!</h1>
+            <p>Znajdziesz tutaj informacje o najlepszych łowiskach, opinie innych wędkarzy oraz wiele innych przydatnych funkcji.</p>
+            <p>Zapraszamy do korzystania z naszej platformy i dzielenia się swoimi doświadczeniami wędkarskimi!</p>
+        </div>
+    );
+}
+
+export default WebPage;
