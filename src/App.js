6644767f92eaa3c1ee1bf9690c59ace82414bd25
Kamil <kamil6395@gmil.com>
2026-03-06 17:48:07 +0100

    SignIn as Index page. New webpage route.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import SignIn from './components/SignComponent/SignIn';
 import SignUp from './components/SignComponent/SignUp';
+import WebPage from './components/WebPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signin" element={<SignIn />} />
+        <Route path="/" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/webpage" element={<WebPage/>} />
       </Routes>
     </BrowserRouter>
   );
