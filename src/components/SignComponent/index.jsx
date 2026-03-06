51f5f59ba8a4226403f284cb219575ac62e24139
Kamil <kamil6395@gmil.com>
2026-03-06 16:16:57 +0100

    Button API fetch.

--- src/components/SignComponent/index.jsx
+++ src/components/SignComponent/index.jsx
@@ -1,7 +1,11 @@
+import React from 'react';
 import { Button, Stack, TextField, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const SignComponent = ({SignType}) => {
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [confirmPassword, setConfirmPassword] = React.useState('');
   return (
     <Stack direction="row" justifyContent="center" alignItems="center" style={{minHeight: '100vh'}}>
         <Stack direction="column" justifyContent="center" alignItems="center" spacing={2} style={{minHeight: '100vh'}}>
@@ -14,13 +18,57 @@ const SignComponent = ({SignType}) => {
                 <Typography variant="h2" align="center">
                 {SignType}
                 </Typography>
-                <TextField label="Email" variant="outlined" fullWidth margin="normal" />
-                <TextField label="Password" variant="outlined" fullWidth margin="normal" type="password" />
+                <TextField label="Email" variant="outlined" fullWidth margin="normal" onChange={(e) => setEmail(e.target.value)} />
+                <TextField label="Password" variant="outlined" fullWidth margin="normal" type="password" onChange={(e) => setPassword(e.target.value)} />
                 {SignType === "Sign Up" && (
-                    <TextField label="Confirm Password" variant="outlined" fullWidth margin="normal" type="password" />
+                    <TextField label="Confirm Password" variant="outlined" fullWidth margin="normal" type="password" onChange={(e) => setConfirmPassword(e.target.value)}/>
                 )}
-                <Button variant="contained" color="primary" fullWidth>
-                    Sign In
+                <Button variant="contained" color="primary" fullWidth onClick={async () => {
+                    if(SignType === "Sign In") {
+                        fetch('http://localhost:3000/api/users/signin', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({
+                                email: email,
+                                password: password
+                            })
+                        }).then(res => res.json())
+                        .then(data => {
+                            if(data.token) {
+                                localStorage.setItem('token', data.token);
+                                alert("Sign In successful!");
+                            } else {
+                                alert("Sign In failed!");
+                            }
+                        })
+                    } else {
+                        if(password !== confirmPassword) {
+                            alert("Passwords do not match!");
+                            return;
+                        }
+                        fetch('http://localhost:3000/api/users/signup', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({
+                                email: email,
+                                password: password
+                            })
+                        }).then(res => res.json())
+                        .then(data => {
+                            if(data.token) {
+                                localStorage.setItem('token', data.token);
+                                alert("Sign Up successful!");
+                            } else {
+                                alert("Sign Up failed!");
+                            }
+                        })
+                    }
+                }}>
+                    {SignType}
                 </Button>
                 {SignType === "Sign In" ? (
                     <Typography variant="body2" align="center">
