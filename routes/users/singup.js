1ec62f59024cb765dcf7fc296e030b23bc0a9a22
Kamil <kamil6395@gmil.com>
2026-03-06 17:18:29 +0100

    Creating user with token, hash password, setting static name

---
+++ routes/users/singup.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../../models/user');
+const bcrypt = require('bcrypt');
+const { createToken } = require('../../utils/JWT_Token');
+const { render } = require('../../app');
+
+router.post("/", async (req, res) => {
+  const { email, password } = req.body;
+  const nazwa = email.split('@')[0];
+  await bcrypt.hash(password, 10, async (err, hash) => {
+    if (err) {
+      return res.status(500).json({ error: err });
+    }
+    const newUser = new User({
+      nazwa: nazwa,
+      misato: null,
+      email: email,
+      haslo: hash,
+      posiadaneLicencje: [],
+      iloscOpinii: 0,
+      ulubioneLowiska: [],
+      czlonePZW: null
+    });
+
+    const accesToken = createToken(newUser);
+    newUser.save()
+      .then(user =>res.status(201).cookie("LegalneLowiskaToken", accesToken, {maxAge:60*60*24*30*100}).json({ message: "User created successfully", user }))
+      .catch(error => res.status(500).json({ error: error }));
+
+  });
+});
+
+module.exports = router;
