75504d7c59adab7ad1b5a8f9d3628a255c03aef2
Kamil <kamil6395@gmil.com>
2026-03-10 10:44:50 +0100

    Verify Token. Get user info

--- routes/users/readUser.js
+++ routes/users/readUser.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../../models/uzytkownik');
 const bcrypt = require('bcrypt');
 const { createToken, verifyToken, verifyAdminToken } = require('../../utils/JWT_Token');
+const jwt = require('jsonwebtoken');
 
 router.post("/", async (req ,res) => {
   const {email, password} = req.body;
@@ -26,9 +27,11 @@ router.get("/", verifyAdminToken, async (req, res) => {
     }).catch((err) => res.status(500).json({message: "Error retrieving users!", error: err}));
 });
 
-router.get("/:id", verifyAdminToken, async (req, res) => {
+router.get("/:id", verifyToken, async (req, res) => {
     const {id} = req.params;
-    await User.findById(id).then((user) => {
+    const decodedToken = jwt.decode(req.cookies["LegalneLowiskaToken"]);
+    if(decodedToken.id !== id) return res.status(403).json({message: "You can only access your own user data!"});
+    await User.findById(decodedToken.id).then((user) => {
         if(!user) return res.status(404).json({message: "User not found!"});
         res.status(200).json({message: "User retrieved successfully!", user});
     }).catch((err) => res.status(500).json({message: "Error retrieving user!", error: err}));
