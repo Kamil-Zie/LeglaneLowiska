a79b1aef0a4b3a72e18151c7eb7925476811cffc
Kamil <kamil6395@gmil.com>
2026-03-10 09:13:07 +0100

    JWT admin auth. Role in token

--- utils/JWT_Token.js
+++ utils/JWT_Token.js
@@ -2,7 +2,7 @@ const {sign, verify} = require("jsonwebtoken");
 
 const createToken = (user) => {
     const accessToken = sign({
-        id: user._id, email: user.email},
+        id: user._id, email: user.email, role: user.rola},
         process.env.JWT_SECRET);
     return accessToken;
 }
@@ -14,6 +14,7 @@ const verifyToken = (req,res,next) => {
         const validToken = verify(accessToken, process.env.JWT_SECRET);
         if(validToken) {
             req.authenticated = true;
+            req.user = validToken;
             return next();
         }
     } catch (err) {
@@ -21,4 +22,21 @@ const verifyToken = (req,res,next) => {
     }
 }
 
-module.exports = { createToken, verifyToken };
+const verifyAdminToken = (req,res,next) => {
+    const accessToken = req.cookies["LegalneLowiskaToken"];
+    if(!accessToken) return res.status(401).json({message: "User not authenticated!"});
+    try {
+        const validToken = verify(accessToken, process.env.JWT_SECRET);
+        if(validToken && validToken.role === "admin") {
+            req.authenticated = true;
+            req.user = validToken;
+            return next();
+        } else {
+            return res.status(403).json({message: "User is not an admin!"});
+        }
+    } catch (err) {
+        return res.status(403).json({message: "Token is not valid!"});
+    }
+}
+
+module.exports = { createToken, verifyToken, verifyAdminToken };
