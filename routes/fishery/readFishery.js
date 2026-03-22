e441300569f933e5139acdb34f75cf1635e76a8c
Kamil <kamil6395@gmil.com>
2026-03-22 22:07:31 +0100

    Get fishery by city

--- routes/fishery/readFishery.js
+++ routes/fishery/readFishery.js
@@ -9,11 +9,11 @@ router.get("/", verifyToken, async (req, res) => {
     }).catch((err) => res.status(500).json({message: "Error retrieving lowiska!", error: err}));
 });
 
-router.get("/:id", async (req, res) => {
-    const {id} = req.params;
-    await Lowisko.findById(id).then((lowisko) => {
-        if(!lowisko) return res.status(404).json({message: "Lowisko not found!"});
-        res.status(200).json({message: "Lowisko retrieved successfully!", lowisko});
+router.get("/:city", async (req, res) => {
+    const {city} = req.params;
+    await Lowisko.find({miasto: {$regex:city,$options:'i'}}).then((lowiska) => {
+        if(!lowiska) return res.status(404).json({message: "Lowisko not found!"});
+        res.status(200).json({message: "Lowisko retrieved successfully!", lowiska});
     }).catch((err) => res.status(500).json({message: "Error retrieving lowisko!", error: err}));
 });
 
