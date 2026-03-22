9bf57ba14ab898dc57e8be6b53954819d2c052d0
Kamil <kamil6395@gmil.com>
2026-03-22 22:09:29 +0100

    Add fishery to fav. no verifyfication

--- routes/users/updateUser.js
+++ routes/users/updateUser.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../../models/uzytkownik');
+const {verifyToken} = require('../../utils/JWT_Token')
+const jwt = require('jsonwebtoken');
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", verifyToken, async (req, res) => {
     const userId = req.params.id;
     const { nazwa, miasto, email, haslo } = req.body;
+    if(verifyToken) {
+        const decodedToken = jwt.decode(req.cookies["LegalneLowiskaToken"]);
+        if(decodedToken.id !== userId) return res.status(403).json({message: "You can only update your own user data!"});
+    }
     try {
         const updatedUser = await User
             .findByIdAndUpdate(userId, { email, password }, { new: true });
@@ -17,7 +23,7 @@ router.put("/:id", async (req, res) => {
     }
 });
 
-router.put("/ulubioneLowisko/:id", async (req, res) => {
+router.post("/favFishery/:id", async (req, res) => {
     const userId = req.params.id;
     const { lowiskoId } = req.body;
     try {
@@ -25,13 +31,17 @@ router.put("/ulubioneLowisko/:id", async (req, res) => {
         if (!user) {
             return res.status(404).json({ message: "User not found!" });
         }
+        if(!lowiskoId)
+            return res.status(404).json({message: "Fishery not found."})
         if (user.ulubioneLowiska.includes(lowiskoId)) {
             user.ulubioneLowiska.pull(lowiskoId);
+            const updatedUser = await user.save();
+            res.status(200).json({ message: "User's favorite fishing spot pulled successfully!", user: updatedUser });
         } else {
             user.ulubioneLowiska.push(lowiskoId);
+            const updatedUser = await user.save();
+            res.status(200).json({ message: "User's favorite fishing spot pushed successfully!", user: updatedUser });
         }
-        const updatedUser = await user.save();
-        res.status(200).json({ message: "User's favorite fishing spot updated successfully!", user: updatedUser });
     } catch (err) {
         res.status(500).json({ message: "Error updating user's favorite fishing spot!", error: err });
     }
