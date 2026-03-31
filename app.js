9fb24cf6517335abe1e45978f25eee8c885e5448
Kamil <kamil6395@gmil.com>
2026-03-31 12:21:31 +0200

    New endpoints, 50MB max request.

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true
 }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Legalne Lowiska Backend is running!');
@@ -44,6 +45,11 @@ app.use('/api/users/signout', require('./routes/users/clearUser'));
 app.use('/api/users/update', require('./routes/users/updateUser'))
 app.use('/api/lowiska', require('./routes/fishery/readFishery'));
 app.use('/api/okregi', require('./routes/district/readDistrict'));
+app.use('/api/licencje', require('./routes/licence/readLicence'));
+app.use('/api/portal/posts', require('./routes/portal/readPosts'));
+app.use('/api/portal/create', require('./routes/portal/createPost'));
+app.use('/api/portal/update', require('./routes/portal/updatePost'));
+app.use('/api/portal/delete', require('./routes/portal/deletePost'));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
